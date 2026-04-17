d13d5890cb9c6c7300ce336e34373d047d6abb65
Amine Essahraoui <amine.essahraoui04@gmail.com>
2026-04-17 22:20:04 +0100

    Generale parametres

--- scripts/process_minipile.py
+++ scripts/process_minipile.py
@@ -3,3 +3,8 @@ from datatrove.executor import LocalPipelineExecutor
 from datatrove.pipeline.readers import HuggingFaceDatasetReader
 from datatrove.pipeline.tokenizers import DocumentTokenizer
 from datatrove.pipeline.writers import JsonlWriter
+
+DATASET_NAME = "JeanKaddour/minipile"   # Miniple dataset
+TOKENIZER_NAME = "core42/jais-13b-chat" # Jais tokenizer
+OUTPUT_PATH = "./output/tokenized_data" # Folder to store tokenized data 
+
